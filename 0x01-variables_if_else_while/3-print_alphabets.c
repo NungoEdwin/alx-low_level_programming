4419194823beb20723229b60e1d00701378fa462
nungo <nued65@gmail.com>
2023-06-19 17:00:55 -0700

    c variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main-writing alphabets starting with small the big consec
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			putchar(a);
+			for (a = 'A'; a <= 'Z'; a++)
+			{
+			putchar(a);
+			putchar("\n");
+			}
+		}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+*Is a directory for variable n control structures*
