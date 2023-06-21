558afa7dd361e2decfe121b8fbb3cb6b015ed3a3
nungo <nued65@gmail.com>
2023-06-20 19:32:49 -0700

    nested

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 			if (n == 98)
 				printf("%d", n);
 			else
-				printf("%d , ", n);
+				printf("%d, ", n);
 		}
 	}
 	else
@@ -23,7 +23,7 @@ void print_to_98(int n)
 			if (n == 98)
 				printf("%d", n);
 			else
-				printf("%d , ", n);
+				printf("%d, ", n);
 		}
 	}
 	putchar('\n');
