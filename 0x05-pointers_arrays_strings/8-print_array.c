82cc0a084c23567001bc2a6a212751ac65f8d86b
nungo <nued65@gmail.com>
2023-06-26 20:14:06 -0700

    pinta

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_array - fxn prnting array
+ *@a:array
+ *@n:array size
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i != n)
+		printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+
+}
+
