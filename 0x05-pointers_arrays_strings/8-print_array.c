872560b3e0eb29b2f169c7519e784b9410554376
nungo <nued65@gmail.com>
2023-06-27 16:21:11 -0700

    pointas

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,14 +8,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n > 0)
+	{
 	for (i = 0; i < n; i++)
 	{
-		if (n == 1)
-			printf("%d\n", a[i]);
-		else if (i != (n - 1))
+		if (i != (n - 1))
 		printf("%d, ", a[i]);
 		else
 			printf("%d\n", a[i]);
+	}
 	}
 
 }
