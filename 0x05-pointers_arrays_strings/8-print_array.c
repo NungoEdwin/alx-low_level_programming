fa227b1d991c7e2a337291f7915d3a4e35067880
nungo <nued65@gmail.com>
2023-06-27 20:08:48 -0700

    p

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,13 +16,13 @@ void print_array(int *a, int n)
 			if (i != --n)
 			{
 				printf("%d, ", a[i]);
-				_putchar('\n');
 			}
 			else if (i == --n)
 			{
 				printf("%d", a[i]);
 			}
 		}
+		_putchar('\n');
 	}
 
 }
