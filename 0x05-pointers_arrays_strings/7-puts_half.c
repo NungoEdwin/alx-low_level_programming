233623e0e6f56523e88ec223d5ba2268373192d4
nungo <nued65@gmail.com>
2023-06-27 18:42:38 -0700

    pointas

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half -hdhdhd
+ * @str:string input
+ */
+void puts_half(char *str)
+{
+	int l, n, i;
+
+	l = strlen(str);
+	if ((l % 2) != 0)
+	{
+		n = (l - 1) / 2;
+		for (i = n; i < l; i++)
+			printf("%c", str[i]);
+	}
+	else if ((l % 2) == 0)
+	{
+		n = l / 2;
+		for (i = n; i < l; i++)
+			printf("%c", str[i]);
+	}
+	_putchar('\n');
+}
+
+
