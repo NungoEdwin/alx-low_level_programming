6fb3988863ae76e564041f8969fdd5ddcc2f036e
nungo <nued65@gmail.com>
2023-07-07 20:01:59 -0700

    static libs

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,8 +1,15 @@
 #include "main.h"
+#include <string.h>
 /**
  * _puts -fxn
  * @s:char
  */
 void _puts(char *s)
 {
+	int i, strl;
+
+	strl = strlen(s);
+
+	for (i = 0 ; i < strl; i++)
+		_putchar(s[i];
 }
