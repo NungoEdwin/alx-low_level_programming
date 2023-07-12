8bd5214d1a5271357f277d1e21f8c52949124768
nungo <nued65@gmail.com>
2023-07-12 16:37:26 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-fxn
+ * * desc: program to add two positive numbers and print
+ *
+ * @argc: parameter argument count
+ * @argv: parameter argument vector
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
