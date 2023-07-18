fc756dba2e4a55b3693f94d386085849265fcabe
nungo <nued65@gmail.com>
2023-07-17 17:10:35 -0700

    typedef n struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,5 @@
 #ifndef dogy
 #define dogy
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 /**
  * struct dog -blabla
  * @name:name
@@ -15,6 +11,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
+
