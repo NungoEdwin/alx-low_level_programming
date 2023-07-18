b95fad7df705c2afc930326a31bc4e7b4f2d2b1c
nungo <nued65@gmail.com>
2023-07-17 19:32:01 -0700

    typedef n structs

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,11 +8,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	d->name = name;
+	if (d)
+	{
+		d->name = name;
 
-	d->age = age;
+		d->age = age;
 
-	d->owner = owner;
+		d->owner = owner;
+	}
 }
