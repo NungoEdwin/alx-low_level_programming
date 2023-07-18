9b9ef348992bf90422a36e3fff3094799cd27965
nungo <nued65@gmail.com>
2023-07-17 19:55:18 -0700

    typedef,structs

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,6 @@ void print_dog(struct dog *d)
 	else if (d->name == NULL)
 		d->name = "(nil)";
 	else if (d->owner == NULL)
-		d->owner = "(nil)"
+		d->owner = "(nil)";
 			printf("Name: %s\nAge: %f\nOwner: %s\n ", d->name, d->age, d->owner);
 }
