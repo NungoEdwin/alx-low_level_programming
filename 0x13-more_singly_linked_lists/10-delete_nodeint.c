8ecabfcc8df33ef92923ad3625127084121331e9
nungo <nued65@gmail.com>
2023-08-02 18:13:23 -0700

    more singly

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - fxn that dels node at a particular position
+ * @head: this is head of linked list
+ * @index: index for loop to delete at
+ * Return: of listint_t type or NULL
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *swap;
+
+	unsigned int i;
+
+	swap = *head;
+
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		swap = swap->next;
+	}
+	if (!swap || swap->next == NULL)
+		return (-1);
+	swap->next = swap->next->next;
+	return (1);
+}
