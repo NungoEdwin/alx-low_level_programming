e3ec0e46d6e2db59952e1301d0231eac868ae3a8
nungo <nued65@gmail.com>
2023-08-02 18:16:44 -0700

    more

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	swap = *head;
 
 
-	for (i = 0; i < (idx - 1); i++)
+	for (i = 0; i < (index - 1); i++)
 	{
 		swap = swap->next;
 	}
